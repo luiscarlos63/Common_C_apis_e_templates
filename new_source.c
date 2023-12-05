a653271d82293ac818be9859b75630bc84ec12ea
LuisConceição-82236-UM <45625746+luiscarlos63@users.noreply.github.com>
2023-12-05 20:33:02 +0000

    Create new_source.c

---
+++ new_source.c
@@ -0,0 +1,28 @@
+/**
+ * @brief This files defines the IO interfaces for the Super Secure Tiles
+ *
+ * @author Valetinho
+ *
+ */
+
+
+/***************************** Include Files *********************************/
+
+#include "sst_io.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/**************************** Type Definitions *******************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/************************** Variable Definitions *****************************/
+
+
+/************************** Function Prototypes ******************************/
+
+
+/*************************** Function Defines ********************************/
